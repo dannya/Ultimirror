17f170119a5182e5ba463c31495f77050797aeb8
Danny Allen <me@dannya.com>
2016-09-17 12:54:55 +0100

    More photos implementation

--- src/modules/photos/photos.js
+++ src/modules/photos/photos.js
@@ -39,7 +39,7 @@ const Photos = UltimirrorModule.extend('Photos', {
         {
             "id":       "user",
             "name":     "Photo service user ID",
-            "initial":  "145207264@N06"
+            "initial":  ""
         }
     ],
 
@@ -51,11 +51,7 @@ const Photos = UltimirrorModule.extend('Photos', {
         self.loading(true);
 
         // initialise request counter
-        var numRequests = 0;
-
         var getRemoteData = function (success, error) {
-            ++numRequests;
-
             // get and process the remote data
             request.post(
                 {
@@ -73,24 +69,25 @@ const Photos = UltimirrorModule.extend('Photos', {
                 },
                 function (err, response, body) {
                     if (!err) {
-                        if (response.statusCode === 200) {
+                        if ((response.statusCode === 200) && body.photos) {
                             // hide loading spinner
                             self.loading(false);
+
+                            // choose a random photo
+                            var randomPhoto = Math.floor(
+                                Math.random() * body.photos.total
+                            );
 
                             // trigger success callback
                             console.log('success for ' + self.moduleType);
 
                             success({
-                                url: body.photos.photo[Math.floor(Math.random() * body.photos.total)].url_l
+                                url: body.photos.photo[randomPhoto].url_l
                             });
 
 
                         } else {
                             // - unknown response code
-                            console.error('--');
-                            console.error(response.statusCode);
-                            console.error(err);
-                            console.error('--');
 
                             // hide loading spinner
                             self.loading(false);
@@ -110,96 +107,6 @@ const Photos = UltimirrorModule.extend('Photos', {
                     }
                 }
             );
-
-
-
-            // $.getJSON(
-            //
-            //     "http://api.flickr.com/services/rest/",
-            //
-            //     {
-            //
-            //         method: 'flickr.people.getPublicPhotos',
-            //
-            //         api_key: apiKey,
-            //
-            //         user_id: the_user_id,
-            //
-            //         format: 'json',
-            //
-            //         nojsoncallback: 1,
-            //
-            //         per_page: 10 // you can increase this to get a bigger array
-            //
-            //     }
-            //
-            // function(data){
-            //
-            //
-            //
-            //     // if everything went good
-            //
-            //     if(data.stat == 'ok'){
-            //
-            //         // get a random id from the array
-            //
-            //         var photoId = data.photos.photo[ Math.floor( Math.random() * data.photos.photo.length ) ];
-            //
-            //
-            //
-            //         // now call the flickr API and get the picture with a nice size
-            //
-            //         $.getJSON(
-            //
-            //             "http://api.flickr.com/services/rest/",
-            //
-            //             {
-            //
-            //                 method: 'flickr.photos.getSizes'.
-            //
-            //                     api_key: apiKey,
-            //
-            //             photo_id: photoId,
-            //
-            //             format: 'json',
-            //
-            //             nojsoncallback: 1
-            //
-            //     }
-            //
-            //         function(response){
-            //
-            //             if(response.stat == 'ok'){
-            //
-            //                 var the_url = response.sizes.size[5].source;
-            //
-            //                 $("#"+your_div_id).append('<img src="' + the_url + '" />');
-            //
-            //             }
-            //
-            //             else{
-            //
-            //                 console.log(" The request to get the picture was not good :\ ")
-            //
-            //             }
-            //
-            //         }
-            //
-            //     );
-            //
-            //
-            //
-            //     }
-            //
-            //     else{
-            //
-            //         console.log(" The request to get the array was not good :( ");
-            //
-            //     }
-            //
-            // }
-            //
-            // );
         };
 
 
